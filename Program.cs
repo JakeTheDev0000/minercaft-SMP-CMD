f73bda064ebcc6c225540148ba1c7df67738b816
jacob <jacobrien64@gmail.com>
2021-10-01 01:29:33 -0700

    added 'close' and 'exit'

--- Program.cs
+++ Program.cs
@@ -1,12 +1,22 @@
 ﻿using System;
 using System.Threading;
 
+using static System.Console;
+ 
+
 namespace minercaft_SMP_CMD
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.Title = "minecraft CMD (BETA)";
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            
+
+            Console.Clear();
+
+            // starts the func
             mainFunc();
 
             // makes sure it does not close
@@ -15,9 +25,26 @@ namespace minercaft_SMP_CMD
 
         static void mainFunc()
         {
-            Console.Write("testyt");
+            Console.Write(">>> ");
 
-            Thread.Sleep(1000);
+            string command = Convert.ToString(Console.ReadLine());
+
+            switch (command)
+            {
+                case "close":
+                Environment.Exit(0);
+                    break;
+
+                case "exit":
+                    Environment.Exit(0);
+                    break;
+                
+                default: 
+                    WriteLine("ERROR!, could not execute command: " + command);
+                    break;
+            }
+
+            
 
             mainFunc();
         }
