78f6fd42e573012cda6702c2304435b377e4f4bd
jacob <jacobrien64@gmail.com>
2021-10-01 00:53:14 -0700

    added all the files

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading;
+
+namespace minercaft_SMP_CMD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            mainFunc();
+
+            // makes sure it does not close
+            Console.ReadKey();
+        }
+
+        static void mainFunc()
+        {
+            Console.Write("testyt");
+
+            Thread.Sleep(1000);
+
+            mainFunc();
+        }
+    }
+}
